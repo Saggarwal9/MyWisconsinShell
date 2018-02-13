8fb3caf605b420b1af202f27ea2b7b985ec0da3f
Saggarwal9 <35288936+Saggarwal9@users.noreply.github.com>
2018-02-12 21:03:52 -0600

    Add files via upload

--- wish.c
+++ wish.c
@@ -3,95 +3,137 @@
 #include<stdlib.h>
 #include<unistd.h>
 #include<sys/wait.h>
-#include <fcntl.h>
+#include<fcntl.h>
+
+int PATHCHANGED=0;
+char *path;
+
 void printError(){
         char error_message[30] = "An error has occurred\n";
         write(STDERR_FILENO, error_message, strlen(error_message));
+        exit(1);
 }
 
 int checkBuiltIn(char *arg){
-   printf("builtIn called\n");
    int flag=0;
-   if(strcmp("exit",arg)==0 || strcmp("cd",arg)==0 || strcmp("path",arg)==0)
+   if(strcmp("exit",arg)==0)
         flag=1;
+   else if(strcmp("cd",arg)==0)
+        flag=2; 
+   else if(strcmp("path",arg)==0)
+        flag=3;
    return flag; 
         
 }
 
 
 
-int newProcess(int myargc,char *myargv[]){
-        int rc=fork();
-        if(rc<0){ //Fork Error
-                printError();
-                exit(1); 
-        }
-        else if(rc==0){ //Child process
-                char *path=(char*)malloc(128);
-                path=strdup("/bin/");
-                path=strcat(path,myargv[0]);
-                if(access(path,X_OK)==0){
-                        if(execv(path,myargv)==-1){
+//TODO: IMPLEMENT REDIRECTION
+//TODO: IMPLEMENT PARALLEL COMMANDS 
+
+
+
+int newProcess(int myargc,char *myargv[]){ //TODO: FIX INDENTATION
+                        int rc=fork();
+                        if(rc<0){ //Fork Error
                                 printError();
-                                exit(1);
-                        }     
-                }
-                else{
-                        path="/usr/bin/";
-                        path=strcat(path,myargv[0]);
-                        if(access(path,X_OK)==0){
-                                printf("\n\naccessed bin2/\n");
-                                if(execv(path,myargv)==-1){
-                                        printError();
-                                        exit(1);
-                                }     
+                                exit(1); 
+                         }
+                         else if(rc==0){ //Child process
+                                path=strcat(path,myargv[0]);
+                                if(access(path,X_OK)==0){//successfully accessed binary or not?
+                                        if(execv(path,myargv)==-1){//successfuly executed binary or not? 
+                                                printError();
+                                                exit(1);
+                                        }     
+                                }
+                                else{
+                                        if(PATHCHANGED==0)
+                                                path="/usr/bin/";
+                                        path=strcat(path,myargv[0]);
+                                        if(access(path,X_OK)==0){//successfully accessed binary or not?
+                                                printf("\n\naccessed bin2/\n");
+                                                if(execv(path,myargv)==-1){//successfuly executed binary or not?
+                                                        printError();
+                                                        exit(1);
+                                                }     
+                                        }
+                                        else{
+                                                printError();
+                                                exit(1);   
+                                        }
+                                }
+                                return 1; 
                         }
                         else{
-                              printError();
-                              exit(1);   
+                                rc=wait(NULL);
                         }
-                }
-                return 1; 
-        }
-        else{
-                
-                rc=wait(NULL);
-                //printf("STUCK IN PARENT of %d (wc:%d) (pid:%d) \n",rc,wc,(int) getpid());
-        }
-        return 0;
+                        
+       
+                return 0;
+        
 }
 
+
 void loop_wish(){
-        
         char *line;
         size_t len=0;
+        path=(char*)malloc(512);
+        path=strdup("/bin/");
         do{
                 int i=0;
                 printf("wish> ");
                 getline(&line,&len,stdin); //reads the entire STDIN command.
                 char *myargs[sizeof(line)]; //Can't be more than the line's size.
                
-                myargs[0]= strtok(line," "); // first call returns pointer to first part of user_input
+                myargs[0]= strtok(line," \n\t"); // first call returns pointer to first part of user_input
                                              // separated by delim
                 while(myargs[i]!=NULL){
                         i++;
-                        myargs[i]=strtok(NULL," "); // every call with NULL uses saved user_input 
+                        myargs[i]=strtok(NULL," \n\t"); // every call with NULL uses saved user_input 
                                                    // value and returns next substring
                 }
-                myargs[0][strlen(myargs[0])-1]=0;//remove the last \n character
+                if(i==0)
+                        printError();
                 myargs[i+1]=NULL;
-                if(checkBuiltIn(myargs[0])==1){
-                        
-                        //executeBuiltIn(myargs); TODO: IMPLEMENT IT
+                int builtin=checkBuiltIn(myargs[0]);
+                if(builtin!=0){ //Built-In Execution.
+                        if(builtin==1){//exit
+                                if(i==1){
+                                        exit(0);
+                                }
+                                else
+                                        printError();
+                        }
+                        else if(builtin==2){//cd
+                                if(i==2){
+                                        if(chdir(myargs[1])!=0)
+                                                printError();
+                                }
+                                else
+                                        printError();
+                        }
+                        else if(builtin==3){
+                                PATHCHANGED=1;
+                                char *temp=(char*) malloc(512);
+                                temp[0]=0;
+                                for(int x=1;x<i;x++){
+                                        temp=strcat(temp,myargs[x]);
+                                }
+                                path=temp;
+                                printf("%s\n",path);
+                                free(temp);
+                                myargs[0]='\0';
+                        }
+                 
                 }
-                else{
+                else
                         newProcess(i,myargs);
-                     }
                
                 //for(int m=0;m<i;m++){ //DEBUGGING PURPOSES. 
                        // printf("%s\n",myargs[m]);
                 //}    
-        }while(strcasecmp(line,"exit\n")!=0); //\n is used for correct functioning of strcasecmp.
+        }while(1); //\n is used for correct functioning of strcasecmp.
 }
 
 
